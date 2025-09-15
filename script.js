09ea5b98e706a804339f2005c28c594474247aa8
Victor Carvalho <victorcarvalhocontact@gmail.com>
2025-09-14 22:26:16 -0300

    feat: melhorar aparência das mensagens de erro com ícone SVG e micro UX

--- script.js
+++ script.js
@@ -59,13 +59,20 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     function mostrarErro(input, span, mensagem) {
-        span.textContent = mensagem;
+        span.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M12 2a10 10 0 1 0 10 10A10.01142 10.01142 0 0 0 12 2Zm0 14a1.25 1.25 0 1 1 1.25-1.25A1.25142 1.25142 0 0 1 12 16Zm1-4.75a1 1 0 0 1-2 0v-5a1 1 0 0 1 2 0Z"/>
+        </svg>
+        <span>${mensagem}</span>`;
+        span.classList.add('mostrar');
         input.classList.add('input-erro');
         input.setAttribute('aria-invalid', 'true');
     }
 
     function limparErros() {
-        [erroNome, erroNota1, erroNota2].forEach((s) => (s.textContent = ''));
+        [erroNome, erroNota1, erroNota2].forEach((s) => {
+            s.innerHTML = "";
+            s.classList.remove('mostrar');
+        });
         [nomeInput, nota1Input, nota2Input].forEach((i) => {
             i.classList.remove('input-erro');
             i.removeAttribute('aria-invalid');
